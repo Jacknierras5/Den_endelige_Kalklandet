25ee68bdb5c27b111cbe0339da337fed05aa1d65
Jack Nierras Beltran <jacknierras@Jacks-MacBook-Pro.local>
2019-05-06 10:00:58 +0200

    done

---
+++ javascript.js
@@ -0,0 +1,13 @@
+window.addEventListener("load", billede);
+
+function billede() {
+    console.log(billede);
+
+    document.querySelector("h5").addEventListener("click", link)
+}
+
+function link() {
+    console.log(link);
+    location.href = "https://facebook.com";
+
+}
